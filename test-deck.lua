08648facf2ff2aaf5e0b97741c430dacc41a396c
iiiaaa123 <146992858+iiiaaa123@users.noreply.github.com>
2024-10-11 22:18:29 -0400

    Create test-deck.lua

---
+++ test-deck.lua
@@ -0,0 +1,8 @@
+local skin = {
+      name = "Inscryption Deck",
+      suit = "S",
+      texture = "InscryptionCards.png",
+      highContrastTexture = nil,
+      cards = { "J", "Q", "K" }
+}
+return skin
