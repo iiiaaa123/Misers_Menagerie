4e06912e4ee54c6070c2ee0393a2d290bbdd2ed7
iiiaaa123 <146992858+iiiaaa123@users.noreply.github.com>
2024-10-25 20:27:32 -0400

    Delete test-deck.lua

--- test-deck.lua
+++
@@ -1,8 +0,0 @@
-local skin = {
-      name = "Inscryption Deck",
-      suit = "S",
-      texture = "InscryptionCards.png",
-      highContrastTexture = nil,
-      cards = { "J", "Q", "K" }
-}
-return skin
