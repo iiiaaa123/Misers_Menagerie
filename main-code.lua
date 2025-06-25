d7c5bde838e53cb73609bee7a8264b91120a4460
Miser <146992858+iiiaaa123@users.noreply.github.com>
2025-06-24 22:24:46 -0400

    Update main-code.lua

--- main-code.lua
+++ main-code.lua
@@ -348,29 +348,29 @@ SMODS.Back {
     end
 }
 
-SMODS.Consumable {
-	key = 'wheelBUT',
-	set = 'Tarot',
-	loc_txt = {
-		name = "Wheel of Fortune*",
-		text = {
-			"{C:green}#1# in #2#{} chance to add",
-        		"{C:dark_edition}Foil{}, {C:dark_edition}Holographic{}, or",
-        	        "{C:dark_edition}Polychrome{} edition",
-        	        "to a random {C:attention}Joker",
-			"...",
-			"{s:0.5}And a {C:green}#1# in #2#{} chance to{}",
-			"{s:0.5}destroy a random joker.{}",
-			"{s:0.5}This is why you read the fine print!{}",
-		}
-	},
-	config = { extra = { odds = 4 } },
-	atlas = 'Tarot',
-	pos = { x = 6, y = 5 },
-	permaeternal = true,
-	cost = 3,
-	hidden = true,
-}
+--SMODS.Consumable {
+--	key = 'wheelBUT',
+--	set = 'Tarot',
+--	loc_txt = {
+--		name = "Wheel of Fortune*",
+--		text = {
+--			"{C:green}#1# in #2#{} chance to add",
+--        		"{C:dark_edition}Foil{}, {C:dark_edition}Holographic{}, or",
+--        	        "{C:dark_edition}Polychrome{} edition",
+--        	        "to a random {C:attention}Joker",
+--			"...",
+--			"{s:0.5}And a {C:green}#1# in #2#{} chance to{}",
+--			"{s:0.5}destroy a random joker.{}",
+--			"{s:0.5}This is why you read the fine print!{}",
+--		}
+--	},
+--	config = { extra = { odds = 4 } },
+--	atlas = 'Tarot',
+--	pos = { x = 6, y = 5 },
+--	permaeternal = true,
+--	cost = 3,
+--	hidden = true,
+--}
 
 SMODS.Consumable {
 	key = 'nothingness',
